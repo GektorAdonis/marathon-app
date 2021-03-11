3717e734c432a0b9d625a4e4975a75e0cf0fc913
GektorAdonis <60072320+GektorAdonis@users.noreply.github.com>
2021-03-11 21:02:18 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react'
+import Header from './components/Header/Header'
+import Layout from './components/Layout/Layout'
+import Footer from './components/Footer/Footer'
+import backImg1 from './img/bg1.jpg'
+import backImg2 from './img/bg2.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header
+      title="This is title"
+      descr="This is Description!" 
+      />
+      <Layout
+        title="This is title"
+        descr="This is Description!" 
+        urlBg={backImg1}
+      />
+      <Layout 
+        title="This is title"
+        descr="This is Description!"
+        colorBg="red" />
+      <Layout 
+        title="This is title"
+        descr="This is Description!" 
+        urlBg={backImg2}/>
+      <Footer />
     </div>
   );
 }
